676078bd84265abfae8e5031cf9ef83195fffed8
Sattwik13 <sattwikmanna6359@gmail.com>
2025-08-11 03:47:39 +0530

    UI update

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,15 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { Message, Contact } = require('./model.js');
+const dotenv = require('dotenv');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+dotenv.config();
 // Connect MongoDB
-mongoose.connect('mongodb+srv://sattwikmanna6359:whatsapp@whatsapp.ieup5p8.mongodb.net/?retryWrites=true&w=majority&appName=whatsapp', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // WebSocket Setup
 const http = require('http').createServer(app);
